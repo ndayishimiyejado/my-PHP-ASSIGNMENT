c334c774ba64948c90f3bc46f069ffd226b0c267
NDAYISHIMIYEJADO <ndayishimiyejado256@gmail.com>
2023-01-20 18:07:55 +0200

    Jado

---
+++ information.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<u><b> My Personal Identification  </b></u></br> </br> ";
+    echo "My Name is : NDAYISHIMIYE Jean de DIEU  </br> ";
+    echo " My Registration Number is: 21RP02800";
+?>
